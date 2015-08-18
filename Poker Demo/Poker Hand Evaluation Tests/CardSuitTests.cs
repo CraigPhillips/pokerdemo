971a85b8074ae2ea6a0eb30981a27f30289d8c10
Craig Phillips <Craig.Phillips@FrozenExports.net>
2015-08-18 16:48:37 -0700

    Added evaluation project and test shell.

---
+++ Poker Demo/Poker Hand Evaluation Tests/CardSuitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Net.FrozenExports.PokerDemo.PokerHandEvaluation.Tests
+{
+    [TestClass]
+    public class CardSuitTests
+    {
+        [TestMethod]
+        public void SuitContentVerification()
+        {
+
+        }
+    }
+}
---
+++ Poker Demo/Poker Hand Evaluation/CardSuit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.FrozenExports.PokerDemo.PokerHandEvaluation
+{
+    /// <summary>
+    ///     The suits into which any single card will falls.
+    /// </summary>
+    public enum CardSuit
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
+    }
+}
