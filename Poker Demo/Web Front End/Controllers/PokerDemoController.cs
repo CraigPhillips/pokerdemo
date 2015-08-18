50e7ac48c66f83b440d50cc8251c809b1146db93
Craig Phillips <Craig.Phillips@FrozenExports.net>
2015-08-18 16:26:51 -0700

    Initial project creation and setup.

---
+++ Poker Demo/Web Front End/Controllers/PokerDemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Net.FrozenExports.PokerDemo.WebFrontEnd.Controllers
+{
+    /// <summary>
+    ///     Simple controller used to gather information needed for the poker hand
+    ///     input and evaluation pages and then trigger the rendering of those views
+    ///     with the relevant data.
+    /// </summary>
+    public class PokerDemoController : Controller
+    {
+        /// <summary>
+        ///     Retrieves information needed to display the poker hand input page and
+        ///     then triggers the rendering of that page.
+        /// </summary>
+        /// <returns>
+        ///     The view, coupled with data needed to render that view, that the MVC framework should 
+        ///     use to display the poker hand input page.
+        /// </returns>
+        public ActionResult PokerInput()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Poker Demo/Web Front End/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Net.FrozenExports.PokerDemo.WebFrontEnd.MvcApplication" Language="C#" %>
